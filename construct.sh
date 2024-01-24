da7143a756087c2420488a08893c03f7b1522888
Tathya Garg <tathya.garg@gmail.com>
2024-01-24 18:28:47 +0530

    removed binaries and iso files

--- construct.sh
+++ construct.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
 BLUE="\033[0;36m"
+GREEN="\033[0;32m"
 RESET="\033[0m"
+
+echo -e -n "${BLUE}Compiling bootloader .......... "
+nasm -felf32 boot.s -o boot.o
+echo -e "${GREEN}Success${RESET}"
+
+echo -e -n "${BLUE}Building kernel ............... "
 /home/tathya/opt/cross/bin/i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -O2 -Wall -Wextra -ffreestanding
-echo -e "${BLUE}Successfully compiled kernel${RESET}"
+echo -e "${GREEN}Success${RESET}"
+
+echo -e -n "${BLUE}Linking kernel & bootloader ... "
 /home/tathya/opt/cross/bin/i686-elf-gcc -T linker.ld -o seaturtleos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
-echo -e "${BLUE}Successfully linked bootloader and kernel${RESET}"
+echo -e "${GREEN}Success${RESET}"
+
+echo -e -n "${BLUE}Copying binary into isodir .... "
 cp seaturtleos.bin isodir/boot/seaturtleos.bin
-echo -e "${BLUE}Copied binary into boot folder${RESET}"
-grub-mkrescue -o seaturtleos.iso isodir
-echo -e "${BLUE}Successfully made iso image${RESET}"
+echo -e "${GREEN}SUCCESS${RESET}"
+
+echo -e -n "${BLUE}Creating iso images ........... "
+grub-mkrescue -o seaturtleos.iso isodir >/dev/null
+echo -e "${GREEN}Success${RESET}"
+
 qemu-system-i386 -cdrom seaturtleos.iso
