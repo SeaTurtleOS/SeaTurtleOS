06c0405868635a73fdd31ada111badc108f9b28e
Tathya Garg <tathya.garg@gmail.com>
2024-02-01 17:20:11 +0530

    Removed stdfs
    Removed the code for the stdfs library, as it mysteriously caused the system to infinitely loop and not do anything.

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 #include <kernel/tty.h>
-#include <stdfs.h>
 #include <stdint.h>
  
 void kernel_main(void) {
 	terminal_init();
-	char* name = "turts.txt";
-	size_t size = 4096;
-	struct tart_fd fd = tar_create(name, size);
-	tar_close(fd.fd);
+	printf("Hello, World!");
 }
